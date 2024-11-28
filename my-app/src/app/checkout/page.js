1eb5d025b344dff843151857cd3661de1c075300
Hayden Carr <HaydenCarrBusiness@gmail.com>
2024-11-28 23:01:57 +0000

    ??

--- my-app/src/app/checkout/page.js
+++ my-app/src/app/checkout/page.js
@@ -1,10 +1,6 @@
-'use client';
+/*'use client';
 import * as React from 'react';
 import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
@@ -107,4 +103,4 @@ export default function Page() {
         </Container>
       </ThemeProvider>
     );
-}
+}*/
